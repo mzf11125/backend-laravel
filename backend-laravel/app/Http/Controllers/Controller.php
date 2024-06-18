af9971733d42a558f07b451438ef2e73bfab5061
M Zidan Fatonie <77999453+mzf11125@users.noreply.github.com>
2024-06-18 16:13:28 +0000

    post and controller

--- backend-laravel/app/Http/Controllers/Controller.php
+++ backend-laravel/app/Http/Controllers/Controller.php
@@ -2,7 +2,38 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return Post::all();
+    }
+
+    public function store(Request $request)
+    {
+        $post = Post::create($request->all());
+        return $post;
+    }
+
+    public function show($id)
+    {
+        return Post::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->update($request->all());
+        return $post;
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return response()->json(['message' => 'Post deleted']);
+    }
 }
